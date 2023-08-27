158c3aeb2b3b8ab55a5a1fc1cd81fd7e652ce33d
Abhishek Sharma <asharma991@outlook.com>
2023-08-27 22:46:06 +0530

    styles and basic ts file

---
+++ src/index.ts
@@ -0,0 +1,48 @@
+import "./styles/main.scss";
+import profile from "./assets/profile.jpg";
+import awsLogo from "./assets/aws-logo.jpg";
+import cssLogo from "./assets/css-logo.jpg";
+import htmlLogo from "./assets/html-logo.jpg";
+import jsLogo from "./assets/js-logo.jpg";
+import nextJsLogo from "./assets/nextjs-logo.jpg";
+import reactJsLogo from "./assets/react-logo.jpg";
+import tsLogo from "./assets/ts-logo.jpg";
+import twitterLogo from "./assets/twitter-logo.svg";
+import githubLogo from "./assets/github-logo.svg";
+import linkedinLogo from "./assets/linkedin-logo.svg";
+
+const currentPath: string = location.pathname;
+
+const linksArr: NodeListOf<HTMLAnchorElement> =
+  document.getElementsByClassName("link");
+
+const activeLink: HTMLAnchorElement | undefined = Array.from(linksArr).find(
+  (link: HTMLAnchorElement) => link.pathname === currentPath
+);
+
+if (activeLink) {
+  activeLink.classList.add("link-active");
+}
+
+const profileImg: HTMLElement | null = document.getElementById("profileImg");
+profileImg?.setAttribute("src", profile);
+const awsLogoImg: HTMLElement | null = document.getElementById("awsImg");
+awsLogoImg?.setAttribute("src", awsLogo);
+const cssLogoImg: HTMLElement | null = document.getElementById("css3Img");
+cssLogoImg?.setAttribute("src", cssLogo);
+const htmlLogoImg: HTMLElement | null = document.getElementById("html5Img");
+htmlLogoImg?.setAttribute("src", htmlLogo);
+const jsLogoImg: HTMLElement | null = document.getElementById("jsImg");
+jsLogoImg?.setAttribute("src", jsLogo);
+const nextJsLogoImg: HTMLElement | null = document.getElementById("nextImg");
+nextJsLogoImg?.setAttribute("src", nextJsLogo);
+const reactJsLogoImg: HTMLElement | null = document.getElementById("reactImg");
+reactJsLogoImg?.setAttribute("src", reactJsLogo);
+const tsLogoImg: HTMLElement | null = document.getElementById("tsImg");
+tsLogoImg?.setAttribute("src", tsLogo);
+const gitLogoSvg = document.getElementById("gitLogo");
+gitLogoSvg?.setAttribute("src", githubLogo);
+const twitterLogoSvg = document.getElementById("twitterLogo");
+twitterLogoSvg?.setAttribute("src", twitterLogo);
+const linkedinLogoSvg = document.getElementById("linkedinLogo");
+linkedinLogoSvg?.setAttribute("src", linkedinLogo);
